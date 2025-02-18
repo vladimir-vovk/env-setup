7f990d2f2b0f2ad73a948d12582acd65b8e6c022
Vladimir Vovk <vladimir.vovk.at@gmail.com>
2025-02-18 20:18:35 +0300

    feat: update shell-utils

--- scripts/shell-utils.sh
+++ scripts/shell-utils.sh
@@ -11,9 +11,9 @@ log "install fzf zoxide yazi..."
 run brew install fzf zoxide yazi
 
 log "install fonts..."
-run brew install font-fira-code font-fira-code-nerd-font font-source-code-pro
+run brew install font-fira-code font-fira-code-nerd-font font-source-code-pro font-consolas-for-powerline
 
 log "install other..."
-run brew install htop btop ffmpeg httpie lazygit ispell neofetch mas mc rsync scrcpy sevenzip tree yt-dlp ripgrep fd
+run brew install htop btop ffmpeg httpie lazygit ispell neofetch mas mc rsync scrcpy sevenzip tree yt-dlp ripgrep fd bat catimg gnupg the_silver_searcher
 
 finish
